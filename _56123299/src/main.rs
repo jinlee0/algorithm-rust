52b3dec2b1f6d5d31d5e2ba257b071edc1bd0cba
jinlee0 <wls4370@gmail.com>
2023-02-26 00:11:07 +0900

    hello rust again

--- _56123299/src/main.rs
+++ _56123299/src/main.rs
@@ -4,8 +4,8 @@ use std::usize;
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let mut numbers = input.split_ascii_whitespace().map(str::parse).flatten();
-    let a: usize = numbers.next().unwrap();
-    let b = numbers.next().unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let a = input.next().unwrap();
+    let b = input.next().unwrap();
     println!("{}", a + b);
 }
